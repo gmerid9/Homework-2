d11ef2f2a3f45d9f91e3f97b8973a2e930a26f4b
Gelila Merid <lilabmeri1@gmail.com>
2022-08-15 15:03:59 -0400

    fourth commit

--- answer.js
+++ answer.js
@@ -73,18 +73,53 @@ console.log(favMovies[8])
 
 favMovies.sort()   //// 
 // console.log(favMovies) 
-favMovies.pop()
-favMovies.push('Guardians of the Galaxy')
+favMovies.pop()     ///// remove the last element in the array 
+favMovies.push('Guardians of the Galaxy') //// push an elment to the end of the array 
 // console.log(favMovies)
-favMovies.reverse()
+favMovies.reverse()    
 // console.log(favMovies)
 favMovies.unshift('The lion King')
 // console.log(favMovies)
-let index =favMovies.index('Django Unchained')
-console.log(index)
-
-// favMovies.splice('d')
+ 
+ console.log(favMovies.indexOf('Django Unchained')) ////get an index of Django Unchained'
 
 
+favMovies.splice(16,0,'Avatar')   ///splice Django Unchained and add Avatar 
+// console.log(favMovies)
 
-//////////////////////////////////////////////////////////
+console.log(favMovies.length)  ///// helps determine the  number of elemets in the array 
+
+ let newFavMovies =favMovies.slice(11)    /////slice to the last half of the array and store it in a new variable 
+console.log(newFavMovies)  
+
+// console.log(indexOf'Fast and Furious') ////error message missing because the array has been permanently altered 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////where is waldo 
+
+const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+                    ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    ["Petunia", ["Baked Goods", "Waldo"]]];
+                    
+whereIsWaldo.splice(1,1 )    //Remove Eggbert
+// console.log(whereIsWaldo)
+whereIsWaldo[1][2]= 'No One'  //cahnge 'Neff' to 'No One'
+// console.log(whereIsWaldo)
+console.log(whereIsWaldo[2][1][1])  //Acess and Console.log 'Waldo'
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////Excited Kitten
+// 1
+const message = 'Love me ,pet me! HSSSSSSS! '
+
+for ( i=0;  i <=20 ; i++) {
+    return message
+}
+console.log(i)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
